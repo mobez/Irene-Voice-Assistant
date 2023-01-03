12a625405a41f7a23e230cd32b6d9577c71aee3b
mobez <aanik@bk.ru>
2023-01-03 23:46:33 +1000

    add plagin aplay play wave to aplay

--- plugins/plugin_tts_pyttsx.py
+++ plugins/plugin_tts_pyttsx.py
@@ -6,6 +6,17 @@ import os
 from vacore import VACore
 import pyttsx3
 
+engine = pyttsx3.init()    # Инициализировать голосовой движок.
+voices = engine.getProperty('voices')
+
+for voice in voices:    # голоса и параметры каждого
+    print('------')
+    print(f'Имя: {voice.name}')
+    print(f'ID: {voice.id}')
+    print(f'Язык(и): {voice.languages}')
+    print(f'Пол: {voice.gender}')
+    print(f'Возраст: {voice.age}')
+
 modname = os.path.basename(__file__)[:-3] # calculating modname
 
 # функция на старте
